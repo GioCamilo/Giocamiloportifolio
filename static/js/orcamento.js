8b887c28f8dcbe35331e388bab25ad17d611d352
Giovanni Camilo Souza Silva <gc.silva@mangels.com.br>
2025-11-12 09:16:38 -0300

    testando email

--- static/js/orcamento.js
+++ static/js/orcamento.js
@@ -5,11 +5,17 @@
   function enviarEmail(event) {
     event.preventDefault();
 
+    const msg = document.getElementById("statusMsg");
+
     emailjs.sendForm("service_z3prova", "template_4a13sob", event.target)
-      .then(function() {
-        alert("✅ Mensagem enviada com sucesso! Em breve entraremos em contato.");
+    .then(() => {
+        msg.textContent = "✅ Mensagem enviada com sucesso! Em breve entraremos em contato.";
+        msg.style.color = "green";
         event.target.reset();
-      }, function(error) {
-        alert("❌ Erro ao enviar mensagem: " + JSON.stringify(error));
-      });
+    })
+    .catch((error) => {
+        msg.textContent = "❌ Erro ao enviar: " + error.text;
+        msg.style.color = "red";
+    });
+
   }
