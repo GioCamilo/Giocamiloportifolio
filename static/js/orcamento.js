0f797082dc3a1e010974680873f157756fad2c04
Giovanni Camilo Souza Silva <gc.silva@mangels.com.br>
2025-11-12 09:08:27 -0300

    adicionando envio para E-mail e orçamento

--- static/js/orcamento.js
+++ static/js/orcamento.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () { 
-    console.log('orcamento.js carregado com sucesso');
+(function(){
+    emailjs.init("yNkZwh8swnikQxpXD"); // substitua pela sua public key do EmailJS
+  })();
 
+  function enviarEmail(event) {
+    event.preventDefault();
 
-
-
-
-});
+    emailjs.sendForm("service_z3prova", "template_4a13sob", event.target)
+      .then(function() {
+        alert("✅ Mensagem enviada com sucesso! Em breve entraremos em contato.");
+        event.target.reset();
+      }, function(error) {
+        alert("❌ Erro ao enviar mensagem: " + JSON.stringify(error));
+      });
+  }
